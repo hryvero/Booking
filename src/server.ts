17c7b9ac58aa9f32ef4da3f4bb4a1433d22cbe93
Veronika Hryhoriv <grigorivveronika@gmail.com>
2023-07-27 00:23:05 +0300

    feat: updated logic of work

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,50 @@
-import express from 'express';
-import http from 'http';
-import mongoose from 'mongoose';
-import { config } from './configs/config';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import fetch from 'node-fetch';
 
-const router = express();
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+    getPerson(id: Int!): Ticket
+  }
 
-mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority' })
-  .then(() => { console.log("Connected") })
-  .catch((error) => { console.log(error) });
+
+  type Ticket {
+    section: String
+    row: Int
+    seatNumber: Int
+    price: Int
+  }
+`;
+
+const resolveFilms = (parent: any) => {
+  const promises = parent.films.map(async (url: string) => {
+    const response = await fetch(url);
+    return response.json();
+  });
+
+  return Promise.all(promises);
+};
+
+const resolvers = {
+
+  Query: {
+    getPerson: async (parent: any, args: any) => {
+      try {
+        console.log(args)
+        const response = await fetch(`https://my.laphil.com/en/rest-proxy/TXN/Packages/${args.id}`);
+        console.log(response.toString());
+        return response.toString();
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server,
+  { listen: { port: 4000 } })
+
+console.log(`🚀 Server ready at ${url}`)
